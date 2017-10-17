ed07325fd5021030ffb071c464174a59be6d8c88
Stacy Webb <swebb@valcom.com>
2017-10-17 01:58:38 -0400

    bug fixes

--- modules/fullvideo/fullvideo.js
+++ modules/fullvideo/fullvideo.js
@@ -15,26 +15,37 @@
 },
 */
 // full_video = "https://i.giphy.com/media/xT9DPKLUNd7GJi642A/giphy.mp4";
-Module.register("fullvideo",{
-	// Default module config.
-	defaults: {
-		full_video: './modules/fullvideo/windygrass.mp4',
-		//loop: true,
-	},
-	getScripts: function() {
-		return ["bundle.js"];
-	},
-	// get stylesheet
-	getStyles: function() {
-		return ["wtc-controller-video.css"];
-	},
-	// Override dom generator.
-	getDom: function() {
-		var video =  document.createElement("video");
-		video.classname = 'muted loop playsinline data-controller="Video" data-fullscreen="true" data-fallback="./modules/fullvideo/poster.png"'
-		video.src = this.config.full_video;
-		video.autoplay = true;
-		//video.loop = this.config.loop;
-		return video;
-	}
+Module.register("fullvideo", {
+  // Default module config.
+  defaults: {
+    full_video: './modules/fullvideo/windygrass.mp4',
+    loop: true,
+  },
+  start: function() {
+    Log.info('Starting module: ' + this.name);
+  },
+  getScripts: function() {
+    return ["wtc-controller-video.js",
+           "bundle.js"];
+  },
+  // get stylesheet
+  getStyles: function() {
+    return ["wtc-controller-video.css"];
+  },
+
+  // Override dom generator.
+  getDom: function() {
+    var wrapper = document.createElement("video");
+    wrapper.setAttribute('muted','');
+    wrapper.setAttribute('playsinline', '');
+    wrapper.setAttribute('loop', '');
+    wrapper.setAttribute('data-controller','Video');
+    wrapper.setAttribute('data-fullscreen','true');
+    var videoelm = document.createElement("source");
+    videoelm.src = this.config.full_video;
+    videoelm.type = "video/mp4";
+    wrapper.appendChild(videoelm);
+
+    return wrapper;
+  }
 });
